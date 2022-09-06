f04f3efba5ef8974ef307fc4ceebac76ff259b34
mahesh <mahesh@192.168.1.7>
2022-09-07 02:20:00 +0530

    clear Error

--- Daily_Work/src/Class_1/A.java
+++ Daily_Work/src/Class_1/A.java
@@ -1,8 +1,17 @@
 package Class_1;
 
 public class A {
-      public static void main (String[]args) {
-          Gk a1 = new Gk ();
-          System.out.println(a1);
-      }
+	public static void main(String[] args) {
+		A a1 = new A();
+		System.out.println(a1);
+		
+		A a2 = new A();
+		System.out.println(a2);
+		
+		A a3 = new A();
+		System.out.println(a3);
+		
+		A a4 = new A();
+		System.out.println(a4);
+	}
 }
