cc7d30318c86ba84c631ce2850879d23e2a02aea
mahesh <mahesh@192.168.1.7>
2022-09-07 05:14:10 +0530

    End 3rd class

--- Daily_Work/Class_3/Class_3/AStat.java
+++ Daily_Work/Class_3/Class_3/AStat.java
@@ -1,14 +1,21 @@
-package Class_3;
+//package Class_3;
+//
+//public class AStat {
+//
+//	static int x = 10;
+//
+//	public static void main(String[] args) {
+//		System.out.println(AStat.x);//10
+//		AStat.x =1000;
+//		System.out.println(AStat.x);//1000
+//
+//	}
+//
+//}
 
-public class AStat {
+//**********************************//
 
-	static int x = 10;
 
-	public static void main(String[] args) {
-		System.out.println(AStat.x);//10
-		AStat.x =1000;
-		System.out.println(AStat.x);//1000
 
-	}
 
-}
+
