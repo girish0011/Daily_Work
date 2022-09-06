b472d22dd16a988ed5088a8e9e6ec5fa266e183f
mahesh <mahesh@192.168.1.7>
2022-09-07 04:16:38 +0530

    Add 3rd class

--- Daily_Work/Class_2/Class_2/A.java
+++ Daily_Work/Class_2/Class_2/A.java
@@ -5,7 +5,11 @@ public class A {
 	static int y = 100; // Static
 	
 	public static void main(String[] args) {
+		A a1 = new A ();
+		System.out.println(a1.x);
+		System.out.println(A.y);
 
 	}
+	
 
 }
---
+++ Daily_Work/Class_3/Class_3/A.java
@@ -0,0 +1,5 @@
+package Class_3;
+
+public class A {
+
+}
