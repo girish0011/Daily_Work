52c65bd7a79af97b13e672ad2b4f926818da4c55
mahesh <mahesh@192.168.1.7>
2022-09-07 03:12:19 +0530

    Working on Second class

---
+++ Daily_Work/Class_1/Class_1/A.java
@@ -0,0 +1,13 @@
+package Class_1;
+
+public class A {
+    public static void main (String[]args){
+    
+    Gk a1 = new Gk();
+    
+    //here is the change in first program the class name is A and in this
+    //program the class name is Gk.
+    
+    System.out.println(a1);
+    }
+   }
